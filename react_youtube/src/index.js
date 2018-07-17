0a4ce46116e203b716b8ae502ac314c18324a3be
AMutaa <piusandru@gmail.com>
2018-07-17 11:55:33 -0400

    env set-up

--- react_youtube/src/index.js
+++ react_youtube/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends Component {
+  render() {
+    return <div>Hi!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#container'));
