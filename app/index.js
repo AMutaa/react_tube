e574995bf6c91544948624c475f09a5ad3a767b4
AMutaa <piusandru@gmail.com>
2018-07-26 17:38:19 -0400

    production

---
+++ app/index.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+import App from './Components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var HtmlWebapackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebapackPlugin({
+      template: 'app/index.html',
+    }),
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
+
+module.exports = config;
+
+
